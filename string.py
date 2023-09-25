fe84c5f5bf99e6091edb6373fdb233583ff7aaba
kusumaPreetham <145997245+kusumaPreetham@users.noreply.github.com>
2023-09-25 19:09:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,55 @@
+#string
+#implicit
+name="preetham"
+print(name)
+print(type(name))
+
+#explicit
+name=str("preetham")
+print(name)
+print(type(name))
+
+#data type annoation
+name:str="preetham"
+print(name)
+print(type(name))
+
+#octal
+a = str(0o123)
+print(a)
+print(type(a))
+
+##binary
+a = str(0b11001)
+print(a)
+print(type(a))
+
+#hexadecmial
+a = str(0xbaf)
+print(a)
+print(type(a))
+
+#boolean
+a = str(True)
+print(a)
+print(type(a))
+
+#boolean conversion
+a = bool("abc")
+print(a)
+print(type(a))
+
+#octal conversion
+a = str("abc")
+print(a)
+print(type(a))
+
+#hexa conversion
+a = str("abc")
+print(a)
+print(type(a))
+
+#binary conversion
+a = str("abc")
+print(a)
+print(type(a))
