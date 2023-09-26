3ef26d604fcd5330912263df5e40fa5e4cd6a83c
kusumaPreetham <145997245+kusumaPreetham@users.noreply.github.com>
2023-09-26 19:35:29 +0530

    Add files via upload

---
+++ tran chec.py
@@ -0,0 +1,69 @@
+#transform funcation
+#capitalize
+a=("this is preetham")
+print(a.capitalize())
+
+#title
+a="kps"
+print((a.title()))
+
+#upper
+name="preetham"
+print(name.upper())
+
+#lower
+name="VISHWA TEJA"
+print(name.lower())
+
+#casefold
+a="RAVI TEJA"
+print(a.casefold())
+
+#swap case
+a="preetham kusuma"
+print(a.swapcase())
+
+#string check funcation
+#isnumaric
+a="123"
+print(a.isnumeric())
+
+#isalnum
+a="nm"
+print(a.isalnum())
+
+#isdemial
+b="1"
+print((b.isdecimal()))
+
+#isdigit
+b="52"
+print(b.isdigit())
+
+#isassci
+b="ash"
+print(b.isascii())
+
+#isupper
+c="PREETHAM"
+print(c.isupper())
+
+#islower
+c="vishwa"
+print(c.islower())
+
+#isspace
+c=' '
+print((c.isspace()))
+
+#istitle
+c="Ramya"
+print((c.istitle()))
+
+#isidentifies
+c= 'rakesh'
+print(c.isidentifier())
+
+#isprintable
+c="raja"
+print(c.isprintable())
