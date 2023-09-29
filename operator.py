c31679c4ee8fa2bd6c2abed302edca47e02d3a64
kusumaPreetham <145997245+kusumaPreetham@users.noreply.github.com>
2023-09-29 17:49:46 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,106 @@
+#assigment operator
+#addition
+a= 7
+b = 9
+print(a + b)
+
+#subration
+a= 7
+b = 3
+print(a - b)
+
+#multiplication
+a= 3
+b = 4
+print(a * b)
+
+# division
+a= 6
+b = 1
+print(a / b)
+
+#module
+a= 4
+b = 3
+print(a % b)
+
+#floor diivision
+a= 7
+b = 5
+print(a//b)
+
+# assignment values
+#equal(=)
+a = "preetham.11"
+print(a)
+
+# +=
+b = 102
+b += 4
+print(b)
+
+# -=
+b -= 3
+print(b)
+
+# *=
+b *= 9
+print(b)
+
+# /=
+b /= 1.8
+print(b)
+
+# %=
+b %= 6
+print(b)
+
+# //=
+c = 7594
+c //= 11
+print(c)
+
+#logic operators
+p = 8
+q = 4
+r = 3
+#and operator
+print(p < q & r < q)
+
+# or operator
+print(p < r | r < q)
+
+# not operator
+print(not(p < r | r < q))
+
+# comparision operator
+x = 9
+v = 2
+# ==
+print(x == v)
+
+# <=
+print(x <= v)
+
+# >=
+print(x >= v)
+c = 3
+e = 7
+
+# !=
+print(c != e)
+
+# <
+print(c < e)
+
+# >
+print(c > e)
+
+#bitwise and operator
+print(c & e)
+
+# bitwise or operator
+print(c | e)
+
+# bitwise xor operator
+print(c ^ e)
