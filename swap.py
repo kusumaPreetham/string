229efe4f1a507858eebefa2514e18cf8f2a4e2cc
kusumaPreetham <145997245+kusumaPreetham@users.noreply.github.com>
2023-10-12 23:00:20 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,27 @@
+#swap 2 numbers
+a = 9
+b = 7
+a = a + b
+b = b - a
+print(b)
+print(a)
+
+#swap 2 number with temp variable
+c = 8
+d = 5
+temp = d
+d = c
+c = temp
+print(c)
+print(d)
+
+#fibanoic
+n = 10
+a = 1
+b = 0
+i = 1
+while i<= n:
+    print(b)
+    a = a + b
+    b = a - b
+    i += 1
