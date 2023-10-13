cb732e15593e63a664a55dbfdee2f33815f51179
kusumaPreetham <145997245+kusumaPreetham@users.noreply.github.com>
2023-10-13 15:50:39 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,24 @@
+#string slicing
+name = "preetham"
+print(name[0:3])
+print(name[:])
+
+#negative slicing
+print(name[-8:-5])
+print(name[:])
+
+#reverse string
+name ="preetham"
+print(name[::-3])
+print(name[::-2])
+
+#revese list
+name = ["preetham","rajeev","ahmed","sai"]
+print(name[::-1])
+
+#reverse a list without a list
+name = ["preetham","rajeev","ahmed","sai"]
+name.reverse()
+print(name)
+
+
